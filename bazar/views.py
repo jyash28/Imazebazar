7a692764e45081178b8cc8be23d616f1f429ebc9
jyash28 <jyash548@gmail.com>
2021-01-18 15:42:13 +0530

    firstcommit

---
+++ bazar/views.py
@@ -0,0 +1,34 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.shortcuts import redirect
+from myapp.models import *
+
+def show_about_page(request):
+    print("about page request")
+    name="Learncode with yash"
+
+    data={
+        'name': name
+    }
+    return render(request,"about.html",data)
+
+def show_home_page(request):
+    cats=Category.objects.all()
+
+    images= Image.objects.all()
+    data={'images' : images,'cats':cats}
+    return render(request,"home.html",data)
+
+def show_category_page(request,cid):
+    print(cid)
+    cats=Category.objects.all()
+
+    
+    category=Category.objects.get(pk=cid)
+    
+    images= Image.objects.filter(cat=category)
+    data={'images' : images,'cats':cats}
+    return render(request,"home.html",data)
+
+def home(request):
+    return redirect('/home')
